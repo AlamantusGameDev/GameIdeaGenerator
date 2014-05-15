17271b38d9d1fd277c9b9bc2093cb3a08f017fe4
Robbie Antenesse <gamedev@alamantus.com>
2014-05-15 14:19:11 -0600

    Added Locations and Descriptions. Increased number of Game Types.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($seed == "") {
 	var n1 = n2 = n3 = n4 = 0;	//nouns
 	var v1 = v2 = 0;	//verbs
 	var a1 = a2 = a3 = a4 = 0;	//adjectives
-	var o1 = o2 = 0;	//optional/chance additions
+	var o1 = o2 = o3 = o4 = 0;	//optional/chance additions
 
 	function generatevalues(seed) {
 		var generatedidea = "";
@@ -33,10 +33,10 @@ if ($seed == "") {
 			Math.seedrandom(seed);
 		}
 		else {
-			Math.seedrandom("poop");
+			Math.seedrandom();
 		}
 		
-		gt=Math.floor(Math.random() * 10);	//Gives game type a value between 0 and 9, providing 10 game type options
+		gt=Math.floor(Math.random() * 20);	//Gives game type a value between 0 and 9, providing 10 game type options
 		n1=Math.floor(Math.random() * 100);	//Gives noun1 a value between 0 and 99, providing 100 noun options
 		n2=Math.floor(Math.random() * 100);	//Gives noun2 a value between 0 and 99, providing 100 noun options
 		v1=Math.floor(Math.random() * 100);	//Gives verb1 a value between 0 and 99, providing 100 noun options
@@ -67,7 +67,7 @@ if ($seed == "") {
 		o1=Math.floor(Math.random() * 10);
 		if (o1 < 5) {
 			//remove the possibility of extra/expanded scenarios
-			n3 = n4 = v2 = a3 = a4 = o2 = -1;
+			n3 = n4 = v2 = a3 = a4 = o2 = o3 = -1;
 		}
 		else {
 			n3=Math.floor(Math.random() * 100);	//Gives noun3 a value between 0 and 99, providing 100 noun options
@@ -95,7 +95,11 @@ if ($seed == "") {
 				a4 = Math.floor(Math.random() * 100);
 				a3 = Math.floor(Math.random() * 100);
 			}
+			
+			o3=Math.floor(Math.random() * 10);
 		}
+		
+		o4=Math.floor(Math.random() * 10);	//Select the sentence structure.
 		
 		var types = [];
 		var ajaxCall1 = $.get("values/gametypes.txt", function (data) { types = data.split("\n"); });
@@ -105,25 +109,123 @@ if ($seed == "") {
 		var ajaxCall3 = $.get("values/verbs.txt", function (data) { verbs = data.split("\n"); });
 		var adjectives = [];
 		var ajaxCall4 = $.get("values/adjectives.txt", function (data) { adjectives = data.split("\n"); });
-
-		$.when(ajaxCall1, ajaxCall2, ajaxCall3, ajaxCall4).done(function () {
-			generatedidea += "A ";
-			generatedidea += types[gt] + " ";
-			generatedidea += "game where a ";
-			if (a1 >= 0) generatedidea += adjectives[a1] + " ";
-			generatedidea += nouns[n1] + " ";
-			generatedidea += verbs[v1] + " a ";
-			if (a2 >= 0) generatedidea += adjectives[a2] + " ";
-			generatedidea += nouns[n2];
-			if (n3 >= 0) {
-			generatedidea += " while a ";
-			if (a3 >= 0) {generatedidea += adjectives[a3] + " ";}
-			generatedidea += nouns[n3] + " ";
+		var locations = [];
+		var ajaxCall5 = $.get("values/locations.txt", function (data) { locations = data.split("\n"); });
+		var descriptions = [];
+		var ajaxCall6 = $.get("values/descriptions.txt", function (data) { descriptions = data.split("\n"); });
+		
+		$.when(ajaxCall1, ajaxCall2, ajaxCall3, ajaxCall4, ajaxCall5, ajaxCall6).done(function () {
+			if (types[gt].substr(0,1)==="a" || types[gt].substr(0,1)==="e" || types[gt].substr(0,1)==="i" || types[gt].substr(0,1)==="o" || types[gt].substr(0,1)==="u") {
+				generatedidea += "An ";
+			} else {
+				generatedidea += "A ";
 			}
-			if (v2 >= 0) generatedidea += verbs[v2] + " a ";
+			generatedidea += types[gt] + " game where ";
+			if (a1 >= 0) {
+				if (adjectives[a1].substr(0,1)==="a" || adjectives[a1].substr(0,1)==="e" || adjectives[a1].substr(0,1)==="i" || adjectives[a1].substr(0,1)==="o" || adjectives[a1].substr(0,1)==="u") {
+					generatedidea += "an ";
+				} else {
+					generatedidea += "a ";
+				}
+				generatedidea += adjectives[a1] + " ";
+			} else {
+				if (nouns[n1].substr(0,1)==="a" || nouns[n1].substr(0,1)==="e" || nouns[n1].substr(0,1)==="i" || nouns[n1].substr(0,1)==="o" || nouns[n1].substr(0,1)==="u") {
+					generatedidea += "an ";
+				} else {
+					generatedidea += "a ";
+				}
+			}
+			generatedidea += nouns[n1] + " ";
+			generatedidea += verbs[v1] + " ";
+			if (a2 >= 0) {
+				if (adjectives[a2].substr(0,1)==="a" || adjectives[a2].substr(0,1)==="e" || adjectives[a2].substr(0,1)==="i" || adjectives[a2].substr(0,1)==="o" || adjectives[a2].substr(0,1)==="u") {
+					generatedidea += "an ";
+				} else {
+					generatedidea += "a ";
+				}
+				generatedidea += adjectives[a2] + " ";
+			} else {
+				if (nouns[n2].substr(0,1)==="a" || nouns[n2].substr(0,1)==="e" || nouns[n2].substr(0,1)==="i" || nouns[n2].substr(0,1)==="o" || nouns[n2].substr(0,1)==="u") {
+					generatedidea += "an ";
+				} else {
+					generatedidea += "a ";
+				}
+			}
+			generatedidea += nouns[n2];
+			if (o4 >= 5) {
+				generatedidea += " in ";
+				if (a2 >= 0) {
+					if (descriptions[a2].substr(0,1)==="a" || descriptions[a2].substr(0,1)==="e" || descriptions[a2].substr(0,1)==="i" || descriptions[a2].substr(0,1)==="o" || descriptions[a2].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+					generatedidea += descriptions[a2] + " ";
+				} else {
+					if (locations[n2].substr(0,1)==="a" || locations[n2].substr(0,1)==="e" || locations[n2].substr(0,1)==="i" || locations[n2].substr(0,1)==="o" || locations[n2].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+				}
+				generatedidea += locations[n2];
+			}
+			switch (o3) {
+				case 0: generatedidea += " while ";
+					break;
+				case 1: generatedidea += " because ";
+					break;
+				case 2: generatedidea += ", and ";
+					break;
+				case 3: generatedidea += " even though ";
+					break;
+				case 4: generatedidea += ", but ";
+					break;
+				case 5: generatedidea += ", however ";
+					break;
+				case 6: generatedidea += ", and then ";
+					break;
+				case 7: generatedidea += ". At the same time, ";
+					break;
+				case 8: generatedidea += ", which means that ";
+					break;
+				case 9: generatedidea += " while ";
+					break;
+			}
+			if (n3 >= 0) {
+				if (a3 >= 0) {
+					if (adjectives[a3].substr(0,1)==="a" || adjectives[a3].substr(0,1)==="e" || adjectives[a3].substr(0,1)==="i" || adjectives[a3].substr(0,1)==="o" || adjectives[a3].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+					generatedidea += adjectives[a3] + " ";
+				} else {
+					if (nouns[n3].substr(0,1)==="a" || nouns[n3].substr(0,1)==="e" || nouns[n3].substr(0,1)==="i" || nouns[n3].substr(0,1)==="o" || nouns[n3].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+				}
+				generatedidea += nouns[n3] + " ";
+			}
+			if (v2 >= 0) generatedidea += verbs[v2] + " ";
 			if (n4 >= 0) {
-			if (a4 >= 0) {generatedidea += adjectives[a4] + " ";}
-			generatedidea += nouns[n4];
+				if (a4 >= 0) {
+					if (adjectives[a4].substr(0,1)==="a" || adjectives[a4].substr(0,1)==="e" || adjectives[a4].substr(0,1)==="i" || adjectives[a4].substr(0,1)==="o" || adjectives[a4].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+					generatedidea += adjectives[a4] + " ";
+				} else {
+					if (nouns[n4].substr(0,1)==="a" || nouns[n4].substr(0,1)==="e" || nouns[n4].substr(0,1)==="i" || nouns[n4].substr(0,1)==="o" || nouns[n4].substr(0,1)==="u") {
+						generatedidea += "an ";
+					} else {
+						generatedidea += "a ";
+					}
+				}
+				generatedidea += nouns[n4];
 			}
 			generatedidea += ".<br />";
 
